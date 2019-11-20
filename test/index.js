b193b9fbee50ca2be2265fbf616f450d157618a2
bridgette333 <bb398307@gmail.com>
2019-11-20 15:39:09 -0800

    more cleanup

--- test/index.js
+++ test/index.js
@@ -12,7 +12,7 @@ describe("Sweetgreen homepage", () => {
 
     it("Should load homepage and correct title", async () => {
         await driver.get("https://order.sweetgreen.com/");
-        var title = await driver.getTitle();
+        const title = await driver.getTitle();
         expect(title).to.equal("sweetgreen: Order Online");
         await driver.wait(until.elementLocated(By.xpath("//*[@data-test-id='location-input-text-field']//*[@id='ember1126_search']")), 2000);
     });
@@ -31,24 +31,25 @@ describe("Sweetgreen homepage", () => {
 
     it("Should display images and data about menu items", async () => {
         await driver.wait(until.elementLocated(By.xpath("//*[@data-test-id='menu-product-name-spicy thai chicken bowl']")));
+        
         // check that title is right
-        var productName = await driver.wait(until.elementLocated(By.xpath("//*[@data-test-id='menu-product-name-spicy thai chicken bowl']" +
+        const productName = await driver.wait(until.elementLocated(By.xpath("//*[@data-test-id='menu-product-name-spicy thai chicken bowl']" +
             "//*[@class='product-name h4 capitalize']"))).getText();
         assert.equal(productName, "Spicy Thai Chicken Bowl");
 
         // check that calories are right
-        var calories = await driver.wait(until.elementLocated(By.xpath("//*[@data-test-id='menu-product-name-spicy thai chicken bowl']" +
+        const calories = await driver.wait(until.elementLocated(By.xpath("//*[@data-test-id='menu-product-name-spicy thai chicken bowl']" +
             "//span[@class='product-cals']"))).getText();
         assert.equal(calories, "  -  620 Cal");
 
         // check that description is right
-        var productDescription = await driver.wait(until.elementLocated(By.xpath("//*[@data-test-id='menu-product-name-spicy thai chicken bowl']" +
+        const productDescription = await driver.wait(until.elementLocated(By.xpath("//*[@data-test-id='menu-product-name-spicy thai chicken bowl']" +
             "//p[@class='product-description p-small']"))).getText();
         assert.equal(productDescription, "Organic arugula, organic wild rice, basil, cucumber, organic carrot, shredded cabbage, toasted almonds, " + 
             "crispy rice, blackened chicken thighs, fresh lime squeeze, spicy cashew dressing");
         
         // check that price is right
-        var productCost = await driver.wait(until.elementLocated(By.xpath("//*[@data-test-id='menu-product-name-spicy thai chicken bowl']" +
+        const productCost = await driver.wait(until.elementLocated(By.xpath("//*[@data-test-id='menu-product-name-spicy thai chicken bowl']" +
             "//span[@class='product-cost']"))).getText();
         assert.equal(productCost, "$12.50");
     });
